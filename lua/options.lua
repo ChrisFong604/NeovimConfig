ceded4873dbb49c15eb5e803c32fe52f6c082292
ChrisFong604 <ccffoonngg@gmail.com>
2024-04-01 01:47:50 -0700

    added vim linebreak and textwrap at 80 characters

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,8 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+o.textwidth = 80
+o.wrap = true 
+o.linebreak = true
 -- o.cursorlineopt ='both' -- to enable cursorline!
